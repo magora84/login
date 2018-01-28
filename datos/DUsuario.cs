c7c91a9053f7925246829ce04961d2486a986c66
magora84 <magora84@hotmail.com>
2018-01-27 20:56:03 -0600

    agregamos la clase usuario

---
+++ datos/DUsuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace login.datos
+{
+    class DUsuario
+    {
+        public static bool IniciarSesion(Usuario usuario)
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            return respuesta;
+        }
+    }
+    public class Usuario
+    {
+        public string id { get; set; }
+        public string usuario {get; set;}
+        public string password { get; set; }
+        public string departamento { get; set; }
+        public string estatus { get; set; }
+        public string email { get; set; }
+        public string sucursal { get; set; }
+        public DateTime fecha_creacion { get; set; }
+        public DateTime fecha_actualizacion { get; set; }
+        public string perfil { get; set; }
+    }
+}
