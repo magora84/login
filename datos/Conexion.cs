c659859cc95b5dd3221f9a2ad2f0648249855029
magora84 <magora84@hotmail.com>
2018-01-27 19:33:32 -0600

    clase de conexion a datos

---
+++ datos/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login.datos
+{
+   public static class Conexion
+    {
+        public static SqlConnection conexion = new SqlConnection();
+        public static void Conectar()
+        {
+            try
+            {
+                conexion = new SqlConnection("Data Source=DESKTOP-0BRLGSJ\\SQLEXPRESS;Initial Catalog=PRUEBA;User ID=sa; password=TCQOJC;");
+                conexion.Open();
+            }
+            catch (SqlException e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+        }
+        public static void Desconectar()
+        {
+            try
+            {
+                if (conexion.State == System.Data.ConnectionState.Open) conexion.Close();
+                
+            }
+            catch(SqlException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
